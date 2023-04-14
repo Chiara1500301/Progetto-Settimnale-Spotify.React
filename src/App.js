0ebb4dfe0d559fe1ce7ae9eb26d4ce1a16117c9f
Chiara Andria <123961543+Chiara1500301@users.noreply.github.com>
2023-04-14 17:08:28 +0200

    Add files via upload
    manca il file node-modules

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import AlbumPage from './components/AlbumPage';
+import ArtistPage from './components/ArtistPage';
+import HomePage from './components/HomePage';
+function App() {
+  return (
+    <div className="App">
+      <div>
+   <HomePage></HomePage>
+       </div>
+       {/*<div>
+        <AlbumPage></AlbumPage>
+       </div>
+       <div>
+        <ArtistPage></ArtistPage>
+       </div>*/}
+  </div>
+
+  );
+}
+
+export default App;
